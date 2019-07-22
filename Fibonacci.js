c319681f7a4bd1f397a1461ee4704c4381ca8a2a
ItsShero <43736989+ItsShero@users.noreply.github.com>
2019-07-22 18:19:20 +1200

    Fibonacci function and html/css files

---
+++ Fibonacci.js
@@ -0,0 +1,26 @@
+//Fibonacci function
+function fibonacci(x){
+    let index = 0;
+    let seq=[1];
+
+    //Computes in N time
+    while(x>seq[seq.length-1]){
+
+        if(seq.length <=1)
+            seq.push(1);
+        else{
+            seq.push(seq[index] + seq[index+1]);
+            index++;
+        }
+        if(x<seq[index] + seq[index+1])
+            break;
+    }
+
+    console.log(seq.toString());
+}
+
+
+//Change the variable found in the function call to specify "x"
+//Default console.log for the output
+
+fibonacci(6);
